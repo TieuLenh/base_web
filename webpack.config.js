ca9f4ace169e86e05b00a2fa7e72d5935b33885d
TieuLenh <hieukaito2k5@gmail.com>
2025-08-18 15:38:33 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,24 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./frontend/src/index.js", // File đầu vào
+  entry: "./frontend/src/index.js", // file đầu vào
   output: {
-    path: path.resolve(__dirname, "./frontend/public"), // Thư mục output
-    filename: "bundle.js"
+    path: path.resolve(__dirname, "frontend/public"), // folder chứa bundle
+    filename: "bundle.js", // tên file bundle
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,  // Với file .js và .jsx
-        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,      // các file .js hoặc .jsx
+        exclude: /node_modules/,  // loại trừ node_modules
         use: {
-          loader: "babel-loader" // Dùng Babel biên dịch
+          loader: "babel-loader"
         }
-      },
-      {
-        test: /\.css$/,  // Cho phép import CSS
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/, // Load ảnh
-        type: "asset/resource"
       }
     ]
   },
   resolve: {
-    extensions: [".js", ".jsx"] // Import file không cần ghi đuôi
+    extensions: [".js", ".jsx"] // import file không cần ghi đuôi
   },
-  devServer: {
-    static: path.join(__dirname, "./frontend/public"),
-    compress: true,
-    port: 3000
-  },
-  mode: "development" // hoặc "production"
+  mode: "development"
 };
